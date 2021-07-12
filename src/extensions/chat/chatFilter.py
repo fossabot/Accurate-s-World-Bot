6fd31f92fcf28e0dbf0c7ace1af7b0c0de849596
Hentyhentys <alexandre.turellier@icloud.com>
2021-07-12 17:47:34 +0300

    First attempt at a chat filter

---
+++ src/extensions/chat/chatFilter.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import json
+
+
+class chatFilter(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        """Checks each message sent for bad words
+
+        Args:
+            message (string): Discord message body
+
+        Returns:
+            integer: Exit code
+        """
+        with open("basWords.json", "r") as f:
+            json.load(f)
+            bWordsList = f["bad-words"]
+        if message.content.lower() in bWordsList:
+            message.delete()
+            embed=discord.Embed(title=f"HEY {message.author.mention}", description="NO BAD WORDS :rage:", color=0xff0000)
+            await message.send(f"?warn {message.author.mention}")
+            
+            async def on_message(message):
+                if message.author.name == "Dyno":
+                    await message.reply("thanks fam :sunglasses:")
+                    return 0
+            
+    
+    
+
+
+def setup(bot:commands.Bot):
+    bot.add_cog(chatFilter(bot))
