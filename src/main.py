fce3cbbde64a145e4e341616a4f8613799921356
Hentyhentys <alexandre.turellier@icloud.com>
2021-07-12 16:53:50 +0300

    New fancy error handling system for loading cogs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands.errors import ExtensionAlreadyLoaded, ExtensionFailed, NoEntryPointError
 
 
 client = commands.Bot(command_prefix="!")
@@ -12,29 +13,62 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-        if message.channel.name == "we-love-amogus":
-            if "amogus" not in message.content.lower():
-                try:
-                    message.delete()
-                except Exception as err:
-                    embed = discord.Embed(title="R.I.P Something went wrong!",
-                                        description=f"if you're developer, here's the traceback. ```{err}```")
-                    return 1
-                
-                await message.channel.send("ONLY AMOGUS :rage: :rage:")
-                await message.channel.send(f"?warn {message.author.display_name}")
-                return 0
-            else:
-                return
-        else:
+    """Checks for AMOGUS
+
+    Args:
+        message (string): message
+
+    Returns:
+        integer: Exit code
+    """
+    if message.channel.name == "we-love-amogus":
+        if "amogus" not in message.content.lower():
+            try:
+                message.delete()
+            except Exception as err:
+                embed = discord.Embed(title="R.I.P Something went wrong!",
+                                    description=f"if you're developer, here's the traceback. ```{err}```")
+                return 1
+            
+            await message.channel.send("ONLY AMOGUS :rage: :rage:")
+            await message.channel.send(f"?warn {message.author.display_name}")
             return 0
+        else:
+            return
+    else:
+        return 0
         
         
 @client.command(name="ping", brief="Display Bot ping")
-async def ping(ctx):
+async def get_ping(ctx):
+    """Gets Bot Ping
+
+    Args:
+        ctx (any): context
+
+    Returns:
+        integer: Exit code
+    """
+    
     embed = discord.Embed(title="Pong!", description=client.latency + "ms",)
     await ctx.channel.send(embed=embed)
     return 0
+    
         
+try:
+    client.load_extension("src\extensions\leveling.py")
+    
+except ExtensionAlreadyLoaded:
+    print("[WARN] Cog Already loaded.")
+
+except NoEntryPointError:
+    raise "[FATAL] Cog does not have a setup function!"
+
+except ExtensionFailed as err:
+    with open("DEBUG/TRACE.txt", "w") as f:
+        f.append(err)
+        
+    raise "[FATAL] Extension failed loading (runtime error)! Traceback was stored in DEBUG/TRACE.txt"
+
 
 client.run("")  # <- token goes here
