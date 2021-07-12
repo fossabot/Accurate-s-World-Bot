d28e7b98c0180c58c5e2ccd7222d37d35132aac8
Hentyhentys <alexandre.turellier@icloud.com>
2021-07-12 21:09:31 +0300

    WHY PYTHON WHY

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ext.errors import ExtensionAlreadyLoaded, NoEntryPointError, ExtensionFailed
+from discord.ext.commands.errors import ExtensionAlreadyLoaded, ExtensionFailed, NoEntryPointError
 import json
 
 
@@ -19,10 +19,7 @@ async def ping(ctx):
     embed = discord.Embed(title="Pong!", description=str(client.latency) + "ms", color=0x3029b3)
     await ctx.channel.send(embed=embed)
     return 0
-        
-
-client.run(settings["token"])
-   
+  
         
 try:
     client.load_extension("extensions.leveling")
@@ -39,3 +36,7 @@ except ExtensionFailed as err:
         
     raise "[FATAL] Extension failed loading (runtime error)! Traceback was stored in DEBUG/TRACE.txt"
 
+
+client.run(settings["token"])
+   
+    
