784f52203dd110c9098d1545544d7075bafdb6ae
Hentyhentys <alexandre.turellier@icloud.com>
2021-07-12 20:31:02 +0300

    things

--- src/main.py
+++ src/main.py
@@ -1,59 +1,28 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands.errors import ExtensionAlreadyLoaded, ExtensionFailed, NoEntryPointError
+from discord.ext.errors import ExtensionAlreadyLoaded, NoEntryPointError, ExtensionFailed
+import json
 
 
 client = commands.Bot(command_prefix="!")
+with open("src\SECRET.json", "r")as f:
+    settings = json.load(f)
 
 
 @client.event
 async def on_ready():
     print("Bot is online")
-
-
-@client.event
-async def on_message(message):
-    """Checks for AMOGUS
-
-    Args:
-        message (string): message
-
-    Returns:
-        integer: Exit code
-    """
-    if message.channel.name == "we-love-amogus":
-        if "amogus" not in message.content.lower():
-            try:
-                message.delete()
-            except Exception as err:
-                embed = discord.Embed(title="R.I.P Something went wrong!",
-                                    description=f"if you're developer, here's the traceback. ```{err}```")
-                return 1
-            
-            await message.channel.send("ONLY AMOGUS :rage: :rage:")
-            await message.channel.send(f"?warn {message.author.display_name}")
-            return 0
-        else:
-            return
-    else:
-        return 0
         
         
 @client.command(name="ping", brief="Display Bot ping")
-async def get_ping(ctx):
-    """Gets Bot Ping
-
-    Args:
-        ctx (any): context
-
-    Returns:
-        integer: Exit code
-    """
-    
-    embed = discord.Embed(title="Pong!", description=client.latency + "ms",)
+async def ping(ctx):
+    embed = discord.Embed(title="Pong!", description=str(client.latency) + "ms", color=0x3029b3)
     await ctx.channel.send(embed=embed)
     return 0
-    
+        
+
+client.run(settings["token"])
+   
         
 try:
     client.load_extension("extensions.leveling")
@@ -70,5 +39,3 @@ except ExtensionFailed as err:
         
     raise "[FATAL] Extension failed loading (runtime error)! Traceback was stored in DEBUG/TRACE.txt"
 
-
-client.run("")  # <- token goes here
