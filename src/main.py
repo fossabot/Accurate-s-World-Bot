c67a6decb109243db4296903a0d81928908df207
Hentyhentys <alexandre.turellier@icloud.com>
2021-07-12 20:21:23 +0300

    fixed stuff

--- src/main.py
+++ src/main.py
@@ -11,31 +11,11 @@ with open("src\SECRET.json", "r")as f:
 @client.event
 async def on_ready():
     print("Bot is online")
-
-
-@client.event
-async def on_message(message):
-        if message.channel.name == "we-love-amogus":
-            if "amogus" not in message.content.lower():
-                try:
-                    message.delete()
-                except Exception as err:
-                    embed = discord.Embed(title="R.I.P Something went wrong!",
-                                        description=f"if you're developer, here's the traceback. ```{err}```")
-                    return 1
-                
-                await message.channel.send("ONLY AMOGUS :rage: :rage:")
-                await message.channel.send(f"?warn {message.author.display_name}")
-                return 0
-            else:
-                return
-        else:
-            return 0
         
         
 @client.command(name="ping", brief="Display Bot ping")
 async def ping(ctx):
-    embed = discord.Embed(title="Pong!", description=client.latency + "ms",)
+    embed = discord.Embed(title="Pong!", description=str(client.latency) + "ms", color=0x3029b3)
     await ctx.channel.send(embed=embed)
     return 0
         
